3d8440aa885569e65035e1a8d2ecf204a788ad9c
cristianelena <cristianfabianelena@gmail.com>
2017-08-14 13:25:58 -0300

    Configuración de CSS y JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,30 @@
-const common = {
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const common = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                importLoaders: 1
+                            }
+                        },
+                        "postcss-loader"
+                    ]
+                })
+            },
+            {
+                test: /js$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader"
+            }
+        ]
+    }
 };
 
 const front = {
