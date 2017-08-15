6ff85719feccb2fa4526ffe9f061e587d5468402
cristianelena <cristianfabianelena@gmail.com>
2017-08-14 23:35:22 -0300

    Paso contexto global al Browser

--- src/Template.js
+++ src/Template.js
@@ -1,8 +1,9 @@
-export default markup => `
+export default (markup, context) => `
     <!DOCTYPE html>
         <head>
             <title>Mercado Libre Web App</title>
             <link rel="stylesheet" href="/styles.css">
+            <script>window.__initialStore = ${ JSON.stringify(context) }</script>
             <script src="/bundle.js" defer></script>
         </head>
         <body>
