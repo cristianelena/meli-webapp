400242c05dc9bc54f0b9bf3057f4b633d756054d
cristianelena <cristianfabianelena@gmail.com>
2017-08-14 23:37:25 -0300

    Conexión de datos en las vistas

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -2,10 +2,26 @@ import React, { Component } from "react";
 import "./Detail.css";
 
 class Detail extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            store: isBROWSER ? window.__initialStore : props.staticContext.initialStore
+        };
+    }
+
+    componentDidMount() {
+        if (!this.state.store) {
+            this.setState({
+                store: this.props.staticContext.initialStore
+            });
+        }
+    }
+
     render() {
         return (
             <div className="detail">
-                Detail
+                Detalle: { JSON.stringify(this.state.store) }
             </div>
         );
     }
--- src/views/Result.jsx
+++ src/views/Result.jsx
@@ -2,10 +2,26 @@ import React, { Component } from "react";
 import "./Result.css";
 
 class Result extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            store: isBROWSER ? window.__initialStore : props.staticContext.initialStore
+        };
+    }
+
+    componentDidMount() {
+        if (!this.state.store) {
+            this.setState({
+                store: this.props.staticContext.initialStore
+            });
+        }
+    }
+
     render() {
         return (
             <div className="result">
-                Result
+                Resultados: { JSON.stringify(this.state.store) }
             </div>
         );
     }
