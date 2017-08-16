143f5e95407fde80a451540982f47a2d32bf2baf
cristianelena <cristianfabianelena@gmail.com>
2017-08-16 17:58:25 -0300

    Initial State Seguro

--- src/Template.js
+++ src/Template.js
@@ -1,9 +1,11 @@
+import serialize from 'serialize-javascript';
+
 export default (markup, data) => `
     <!DOCTYPE html>
         <head>
             <title>Mercado Libre Web App</title>
             <link rel="stylesheet" href="/styles.css">
-            <script>window.__initialState = ${ JSON.stringify(data) }</script>
+            <script>window.__initialState = ${ serialize(data) }</script>
             <script src="/bundle.js" defer></script>
         </head>
         <body>
