0d1cf99b2342a42b6596ac3665488e76ea1e0f83
cristianelena <cristianfabianelena@gmail.com>
2017-08-16 15:10:55 -0300

    Redux Browser

--- src/browser.js
+++ src/browser.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
 import App from "./App";
 
+import configureStore from './store';
+const store = configureStore(window.__initialState);
+delete window.__initialState;
+
 render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById("root")
 );
--- src/store.js
+++ src/store.js
@@ -2,6 +2,6 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-const configureStore = preloadedState => createStore(reducer, preloadedState, applyMiddleware(thunk));
+const configureStore = (preloadedState = {}) => createStore(reducer, preloadedState, applyMiddleware(thunk));
 
 export default configureStore;
