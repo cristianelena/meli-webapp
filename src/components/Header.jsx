22c610c7e4550829212164226c4a29d5f02a9d40
cristianelena <cristianfabianelena@gmail.com>
2017-08-17 03:06:16 -0300

    Diseño & Buscador - Componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
+import { withRouter, Link } from 'react-router-dom';
+
 import "./Header.css";
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onSubmit = this.onSearch.bind(this);
+    }
+
+    onSearch(ev) {
+        ev.preventDefault();
+
+        this.props.history.push(`/list/${ this.search.value }`);
+    }
+
     render() {
         return (
             <header className="header">
-                <form action="/list/">
-                    <input type="text" name="search" placeholder="Nunca dejes de buscar" />
-                    <button type="submit">buscar</button>
+                <Link to="/" className="header-logo"><img src="/logo.png" alt="Mercado Libre" /></Link>
+                <form action="/list/" className="header-form" onSubmit={ this.onSubmit }>
+                    <input type="text" name="search" ref={ search => (this.search = search) } placeholder="Nunca dejes de buscar" className="header-input" />
+                    <button type="submit" className="header-submit">buscar</button>
                 </form>
             </header>
         );
     }
 }
 
-export default Header;
+export default withRouter(Header);
