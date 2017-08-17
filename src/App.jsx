63bd3f3bd381e1f428e16ca432aaa313b3f0e9ea
cristianelena <cristianfabianelena@gmail.com>
2017-08-17 03:05:18 -0300

    Diseño Componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import routes from './routes';
-import "./App.css";
-import Header from './components/Header';
 
-const App = () => {
-    return (
-        <div className="app">
-            <Header />
-            <ul>
-                <li><Link to="/">Inicio</Link></li>
-                <li><Link to="/list/iphone">Búsqueda search=iphone</Link></li>
-                <li><Link to="/items/MLA664934899">Detalle ID:MLA664934899</Link></li>
-            </ul>
-            <Switch>
-                { routes.map((route, i) => <Route key={i} {...route} />) }
-            </Switch>
-        </div>
-    );
-};
+import "./App.css";
+import Header from 'components/Header';
+
+const App = () => <div className="app">
+    <Header />
+    <Switch>
+        { routes.map((route, i) => <Route key={i} {...route} />) }
+    </Switch>
+</div>;
 
 export default App;
